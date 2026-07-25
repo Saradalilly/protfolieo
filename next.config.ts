6db569d68ba3874a68f03d6fb6e84f483a54434e
Saradalilly <146159523+Saradalilly@users.noreply.github.com>
2026-07-25 21:21:43 +0530

    fix: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Ensure images from external domains load correctly
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
